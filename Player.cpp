9a9130c5e2e53d6d967644c7b1a7085c6c2c1519
Kenwyn Chin <kenwynchin@yahoo.com>
2026-05-01 22:05:32 +1000

    Added Player class

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
 // Stores Player class (HP, Items, Actions)
+class Person{
+    private:
+        string name {};
+        int health {};
+    public:
+    Person(string name, int health){
+        this->name = name;
+        this->health = health;
+    };
+};
